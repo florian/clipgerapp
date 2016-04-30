615d603fac5e06fa4e27ff8a87f48828a0dfdeb9
Florian Hartmann <jscoderr@gmail.com>
2016-04-30 13:50:44 +0200

    Add 'show more images' functionality

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-$(document).ready(function () {
+$(document).ready(main)
+
+function main () {
+  initMagnificPopup()
+  initShowMoreImages()
+}
+
+function initMagnificPopup () {
   $('.images').magnificPopup({
     delegate: 'a',
     type: 'image',
     gallery: {
       enabled: true
     },
-    mainClass: 'mfp-fade',
     image: {
       titleSrc: function(item) {
         return item.el.attr('title')
       }
     }
   })
-})
+}
+
+function initShowMoreImages () {
+  $('.images ul').addClass('limited')
+
+  $('.show-all-images').show().click(function () {
+    $('.images ul').removeClass('limited')
+    $(this).hide()
+    $('.images')[0].scrollIntoView()
+  })
+}
